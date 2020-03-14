0fb29883a594609e0c045de897410b3eaa9a0030
acer <acer@192.168.124.77>
2020-03-13 21:07:59 -0300

    commit geometrico

--- gui/src/gui/Ponto.java
+++ gui/src/gui/Ponto.java
@@ -21,6 +21,39 @@ public class Ponto implements Formas{
 	private int y;
 	
 	
+	
+	public Ponto()
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public float distancia(Ponto p) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 }
